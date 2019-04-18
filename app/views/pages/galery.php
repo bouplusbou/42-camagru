d49a39eeaada3ae8a0fdaa65cacbb0ff32354b01
Baptiste BOUCHER <bboucher@e3r1p12.42.fr>
2019-04-18 11:12:51 +0200

    show or not like and comment button

--- app/views/pages/galery.php
+++ app/views/pages/galery.php
@@ -8,8 +8,10 @@
     </div>
     <img src="<?= './app/assets/images/post_img/'.$post->photo_name; ?>" alt="">
     <div class="post_footer">
+    <?php if (isset($_SESSION['username']) && isset($_SESSION['id_user'])) { ?>
         <button class="like_btn" id_post="<?= $post->id_post; ?>">Like</button>
         <a href="<?= 'index.php?p=viewPost&id='.$post->id_post; ?>">Comment</a>
+    <?php } ?>
         <div class="likes">
             <p id_post_show_likes="<?= $post->id_post; ?>"><?= $post->likes_count ? $post->likes_count : 0; ?></p>
             <p> Likes</p>
@@ -18,13 +20,13 @@
 </div>
 <?php endforeach; ?>
 
-<script type="text/javascript">
 <?php if (isset($_SESSION['username']) && isset($_SESSION['id_user'])) { ?>
+<script type="text/javascript">
     let currentUsername = "<?= $_SESSION['username']; ?>";
     let currentUserID = "<?= $_SESSION['id_user']; ?>";
-<?php } ?>
 </script>
 <script type="text/javascript" src="./app/assets/js/galery.js"></script>
+<?php } ?>
 
 
 <div id="message" style="color:red;"></div>
--- app/views/pages/viewPost.php
+++ app/views/pages/viewPost.php
@@ -6,7 +6,9 @@
     </div>
     <img src="<?= './app/assets/images/post_img/'.$post['photo_name']; ?>" alt="">
     <div class="post_footer">
+    <?php if (isset($_SESSION['username']) && isset($_SESSION['id_user'])) { ?>
         <button id="like_btn" id_post="<?= $post['id_post']; ?>">Like</button>
+    <?php } ?>
         <div class="likes">
             <p id_post_show_likes="<?= $post['id_post']; ?>"><?= $post['likes_count'] ? $post['likes_count'] : 0; ?></p>
             <p> Likes</p>
@@ -19,17 +21,19 @@
             </div>
         <?php endforeach; ?>
         </div>
+        <?php if (isset($_SESSION['username']) && isset($_SESSION['id_user'])) { ?>
 		<input placeholder="Type your comment here..." type="text" value="" name="comment" id="comment_input" />
 		<input type="submit" value="comment" name="submit" id="comment_btn" id_user="<?= $_SESSION['id_user']; ?>" id_post="<?= $post['id_post']; ?>" />
+        <?php } ?>
     </div>
 </div>
 
 <div id="message" style="color:red;"></div>
 
-<script type="text/javascript">
 <?php if (isset($_SESSION['username']) && isset($_SESSION['id_user'])) { ?>
+<script type="text/javascript">
     let currentUsername = "<?= $_SESSION['username']; ?>";
     let currentUserID = "<?= $_SESSION['id_user']; ?>";
-<?php } ?>
-</script>
+    </script>
 <script type="text/javascript" src="./app/assets/js/viewPost.js"></script>
+<?php } ?>
